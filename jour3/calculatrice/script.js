2c465d6ad59af559d1dfe9ec4efce12af64e9f90
jazz009 <treguier@gmail.com>
2018-04-18 14:17:38 +0200

    calculatrice

---
+++ jour3/calculatrice/script.js
@@ -0,0 +1,29 @@
+// selectionne tous les td et renvoi un tableau
+var x = document.querySelectorAll("td");
+
+//boucle pour parcourir le tableau et ajouter un listener sur le clic
+var i;
+for (i = 0; i < x.length; i++) { 
+
+    var myVal = x[i]
+
+    // console.log(myVal);
+
+    myVal.addEventListener("click",function(){ 
+
+        myTxt = this.textContent;
+
+        // var myInt = parseInt(myTxt);
+        // console.log(typeof myInt);
+        show(myTxt)
+    });
+
+// affiche dans l'ecran la touche cliquée
+function show(myTx){  
+    document.getElementById('screen').textContent = myTxt;
+}
+
+// reste a implementer le calcul
+
+
+}
