d5a41e2ace96c0b411239d31972795d7efb64948
jazz009 <treguier@gmail.com>
2018-04-19 02:18:03 +0200

    modification calculatrice suite amise en place ESlint

--- jour3/calculatrice/script.js
+++ jour3/calculatrice/script.js
@@ -1,29 +1,26 @@
+// ajout de ESlint avec les regles air-bnb https://travishorn.com/setting-up-eslint-on-vs-code-with-airbnb-javascript-style-guide-6eb78a535ba6
 // selectionne tous les td et renvoi un tableau
-var x = document.querySelectorAll("td");
+const x = window.document.querySelectorAll('td');
 
-//boucle pour parcourir le tableau et ajouter un listener sur le clic
-var i;
-for (i = 0; i < x.length; i++) { 
+// boucle pour parcourir le tableau et ajouter un listener sur le clic
+let i;
+for (i = 0; i < x.length; i++) {
+  let myVal = x[i];
 
-	var myVal = x[i];
+  // console.log(myVal);
 
-	// console.log(myVal);
+  myVal.addEventListener('click', function () {
+    let myTxt = this.textContent;
 
-	myVal.addEventListener("click",function(){ 
+    // var myInt = parseInt(myTxt);
+    console.log(typeof myInt);
+    show(myTxt);
+  });
 
-		var	myTxt = this.textContent;
-
-		// var myInt = parseInt(myTxt);
-		// console.log(typeof myInt);
-		show(myTxt);
-	});
-
-	// affiche dans l'ecran la touche cliquée
-	function show(myTx){  
-		document.getElementById("screen").textContent = myTxt;
-	}
+  // affiche dans l'ecran la touche cliquée
+  function show(myTx) {
+    document.getElementById('screen').textContent = myTxt;
+  }
 
 // reste a implementer le calcul
-
-
 }
